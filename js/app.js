d71d0dd107ad6a34bb93f36c772fce9bba6b18ba
doblepapi <jasonbnick@gmial.com>
2020-08-13 09:48:01 +0200

    check if the form is complete

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+const botonEnviar = document.querySelector('#enviar');
+
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', deshabilitarBotonEnviar);
+    email.addEventListener('blur', compruebaFormulario);
+    asunto.addEventListener('blur', compruebaFormulario);
+    mensaje.addEventListener('blur', compruebaFormulario);
+
+}
+
+function compruebaFormulario() {
+    validarLongitud(this);
+    habilitaBotonCamposCompletos();
+    if (this.type === 'email') {
+        validaEmail(this);
+    }
+}
+
+function validarLongitud(campo) {
+    if (campo.value.length > 0) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+        return;
+    }
+    campo.style.borderBottomColor = 'red';
+    campo.classList.add('error');
+    return;
+}
+
+function validaEmail(campo){
+    let email = campo.value;
+    if(email.indexOf('@') !== -1){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+        return;
+    }
+}
+
+function habilitaBotonCamposCompletos() {
+    let errores = document.querySelectorAll('.error');
+    if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+        if (errores.length === 0) {
+            botonEnviar.disabled = false;
+        }
+    }
+}
+
+function deshabilitarBotonEnviar() {
+    botonEnviar.disabled = true;
+}
